1df84d8b62bc43767e1584a21cf66938f1d2a2d1
Lauren McCrystal <lmccrystal@uri.edu>
2024-04-05 11:47:09 -0400

    adding a commit

---
+++ rare_bird_sightings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#making a loop to determine the rare_birds = 1 
+
+for file in beg_birds/*;
+do
+count=$(awk -F',' '$13 == 1 {count++} END {print count}' "$file")
+echo "File: $file, Count: $count"
+done 
