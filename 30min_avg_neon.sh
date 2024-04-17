5353bb63634bea6b7331c975101e4136b15302fd
laurenmccrystal <166147092+laurenmccrystal@users.noreply.github.com>
2024-04-16 21:22:03 -0400

    Update 30min_avg_neon.sh

--- 30min_avg_neon.sh
+++ 30min_avg_neon.sh
@@ -4,3 +4,5 @@
 I a love your changes, this has been a great exam, this is person 2 signing off challenge 3. 
 
 Person 2 accepting the changes of person 1, thank you  edits.   main
+
+This is the final edit of challenge 3 after cloning the repo.   
